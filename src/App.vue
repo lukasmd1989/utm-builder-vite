fcbe65f6c8dcc41dd899fcc893e99197f39d9868
lukasmd1989 <lukas.m.dittmer@me.com>
2024-06-27 13:24:31 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+name: 'App',
+});
+</script>
+
+<style>
+/* Global styles */
+body {
+font-family: Arial, sans-serif;
+background-color: #f5f5f5;
+margin: 0;
+padding: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="home">
+  <UTMBuilder />
+ </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UTMBuilder from '@/components/UTMBuilder.vue';
+
+export default defineComponent({
+name: 'Home',
+components: {
+  UTMBuilder,
+},
+});
+</script>
+
+<style scoped>
+.home {
+padding: 2rem;
+display: flex;
+justify-content: center;
+}
+</style>
